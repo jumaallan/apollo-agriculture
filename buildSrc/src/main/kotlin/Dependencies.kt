f78b9b33e3340d29f79b1ad2c89b67faac83a474
Juma Allan <allanjuma@gmail.com>
2021-08-30 14:25:59 +0300

    added buildSrc setup

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,42 @@
+object Versions {
+
+    // Kotlin
+    const val kotlinVersion = "1.4.21"
+    const val coreKtx = "1.5.0-beta01"
+
+    // DI - KOIN
+    const val koin = "3.1.2"
+
+    // Timber
+    const val timber = "5.0.0"
+
+    // Firebase
+    const val firebase = "26.0.0"
+
+    // test libraries
+    const val espresso = "3.4.0-alpha02"
+    const val annotation = "1.2.0-alpha01"
+
+}
+
+object Libraries {
+
+    // Kotlin
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+
+    // DI - KOIN
+    const val koin = "io.insert-koin:koin-android:${Versions.koin}"
+    const val koinCompose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
+
+    // Timber
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    // Firebase
+    const val bom = "com.google.firebase:firebase-bom:${Versions.firebase}"
+    const val crashlytics = "com.google.firebase:firebase-crashlytics"
+
+    // test libraries
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val annotation = "androidx.annotation:annotation:${Versions.annotation}"
+}
