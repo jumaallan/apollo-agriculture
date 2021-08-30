c442b9af540f729870b03b38856b9f55ca391281
Juma Allan <allanjuma@gmail.com>
2021-08-30 16:54:43 +0300

    cleaning up resources, deps and drawables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,13 +124,8 @@ android {
     }
 
     buildFeatures {
-//        compose = true
         viewBinding = true
     }
-
-//    composeOptions {
-//        kotlinCompilerExtensionVersion = "1.0.1"
-//    }
 
     packagingOptions {
         resources {
@@ -199,12 +194,6 @@ android {
 
         implementation("androidx.appcompat:appcompat:1.3.1")
         implementation("com.google.android.material:material:1.4.0")
-
-//    implementation("androidx.compose.ui:ui:1.1.0")
-//    implementation("androidx.compose.material:material:1.1.0")
-//    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0")
-//    implementation("androidx.activity:activity-compose:1.3.1")
-//    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0")
 
         implementation("androidx.constraintlayout:constraintlayout:2.1.0")
         implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
