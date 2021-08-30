3fbecdd2ec70f74edb928260bebcd327924de4c6
Juma Allan <allanjuma@gmail.com>
2021-08-30 16:21:35 +0300

    setting up application class - init a few things

---
+++ app/src/main/java/com/apolloagriculture/ApolloAgriculture.kt
@@ -0,0 +1,57 @@
+package com.apolloagriculture
+
+import android.app.Application
+import androidx.annotation.Nullable
+import com.apolloagriculture.core.crashlytics.CrashlyticsTree
+import com.apolloagriculture.data.di.dataModules
+import com.apolloagriculture.di.appModules
+import com.apolloagriculture.network.di.networkModules
+import org.jetbrains.annotations.NotNull
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import org.koin.core.logger.Level
+import org.koin.core.module.Module
+import timber.log.Timber
+
+open class ApolloAgriculture : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin()
+        initTimber()
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger(Level.ERROR)
+                androidContext(applicationContext)
+                val modules = mutableListOf<Module>().apply {
+                    addAll(appModules)
+                    addAll(dataModules)
+                    addAll(networkModules)
+                }
+                modules(modules)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
+    }
+
+    private fun initTimber() = when {
+        BuildConfig.DEBUG -> {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+        else -> {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+}
