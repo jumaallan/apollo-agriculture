4dd0650c5dc1b18013a2b28de0d7d7ce8c26c703
Juma Allan <allanjuma@gmail.com>
2021-08-31 15:51:15 +0300

    working on weather card ui

--- app/src/main/java/com/apolloagriculture/views/composables/DayAfterTomorrowCard.kt
+++ app/src/main/java/com/apolloagriculture/views/composables/DayAfterTomorrowCard.kt
@@ -18,10 +18,16 @@ package com.apolloagriculture.views.composables
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.apolloagriculture.R
 import com.apolloagriculture.views.theme.ApolloAgricultureTheme
 
 @Composable
@@ -31,13 +37,107 @@ fun DayAfterTomorrowCard() {
             .fillMaxWidth()
             .padding(15.dp)
             .clickable { },
-        elevation = 10.dp
+        elevation = 12.dp
     ) {
         Column(
             modifier = Modifier.padding(15.dp)
         ) {
-            Spacer(Modifier.size(15.dp))
-            Spacer(Modifier.size(15.dp))
+            Spacer(Modifier.size(6.dp))
+            DayAfterTomorrow()
+            Spacer(Modifier.size(4.dp))
+        }
+    }
+}
+
+@Composable
+fun DayAfterTomorrow() {
+    val isLightTheme =
+        MaterialTheme.colors.isLight // check whether the device’s theme selected is Light mode or not
+    Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center) {
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 6.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "Day After Tomorrow",
+                    style = MaterialTheme.typography.h1,
+                    textAlign = TextAlign.Center
+                )
+                WeatherIconImage(
+                    image =
+                    if (isLightTheme) {
+                        R.drawable.ic_weather_rain
+                    } else {
+                        R.drawable.ic_weather_thunder
+                    },
+                    contentScale = ContentScale.Fit,
+                    contentDesc = "Weather Icon",
+                    modifier = Modifier
+                        .size(44.dp)
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Column(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "32.3",
+                        style = MaterialTheme.typography.subtitle1,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Spacer(modifier = Modifier.size(6.dp))
+                    Text(
+                        text = "Low Temperature",
+                        style = MaterialTheme.typography.body1,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "52.3",
+                        style = MaterialTheme.typography.subtitle1,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Spacer(modifier = Modifier.size(6.dp))
+                    Text(
+                        text = "High Temperature",
+                        style = MaterialTheme.typography.body1,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = "broken clouds",
+                style = MaterialTheme.typography.caption,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -52,7 +152,7 @@ fun DayAfterTomorrowCardPreview() {
 
 @Preview
 @Composable
-fun DayAfterTomorrowCardModePreview() {
+fun DayAfterTomorrowCardDarkModePreview() {
     ApolloAgricultureTheme(darkTheme = true) {
         DayAfterTomorrowCard()
     }
--- app/src/main/java/com/apolloagriculture/views/composables/TomorrowCard.kt
+++ app/src/main/java/com/apolloagriculture/views/composables/TomorrowCard.kt
@@ -18,10 +18,16 @@ package com.apolloagriculture.views.composables
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.apolloagriculture.R
 import com.apolloagriculture.views.theme.ApolloAgricultureTheme
 
 @Composable
@@ -36,8 +42,102 @@ fun TomorrowCard() {
         Column(
             modifier = Modifier.padding(15.dp)
         ) {
-            Spacer(Modifier.size(15.dp))
-            Spacer(Modifier.size(15.dp))
+            Spacer(Modifier.size(6.dp))
+            Tomorrow()
+            Spacer(Modifier.size(4.dp))
+        }
+    }
+}
+
+@Composable
+fun Tomorrow() {
+    val isLightTheme =
+        MaterialTheme.colors.isLight // check whether the device’s theme selected is Light mode or not
+    Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center) {
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 6.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "Tomorrow",
+                    style = MaterialTheme.typography.h1,
+                    textAlign = TextAlign.Center
+                )
+                WeatherIconImage(
+                    image =
+                    if (isLightTheme) {
+                        R.drawable.ic_weather_rain
+                    } else {
+                        R.drawable.ic_weather_thunder
+                    },
+                    contentScale = ContentScale.Fit,
+                    contentDesc = "Weather Icon",
+                    modifier = Modifier
+                        .size(44.dp)
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Column(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "32.3",
+                        style = MaterialTheme.typography.subtitle1,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Spacer(modifier = Modifier.size(6.dp))
+                    Text(
+                        text = "Low Temperature",
+                        style = MaterialTheme.typography.body1,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "52.3",
+                        style = MaterialTheme.typography.subtitle1,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Spacer(modifier = Modifier.size(6.dp))
+                    Text(
+                        text = "High Temperature",
+                        style = MaterialTheme.typography.body1,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = "broken clouds",
+                style = MaterialTheme.typography.caption,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -52,7 +152,7 @@ fun TomorrowCardPreview() {
 
 @Preview
 @Composable
-fun TomorrowCardDarkModePreview() {
+fun TomorrowCardDarkDarkModePreview() {
     ApolloAgricultureTheme(darkTheme = true) {
         TomorrowCard()
     }
