7ac22f75ddcc32fef8bd18f3c92e0eb913adf414
Juma Allan <allanjuma@gmail.com>
2021-08-31 19:23:43 +0300

    added mock web dispatcher, and sample response

---
+++ app/src/test/java/com/apolloagriculture/dispatcher/MockRequestDispatcher.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Apollo Agriculture
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.apolloagriculture.dispatcher
+
+import com.google.common.io.Resources
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
+import java.io.File
+import java.net.HttpURLConnection
+
+open class MockRequestDispatcher : Dispatcher() {
+
+    override fun dispatch(request: RecordedRequest): MockResponse {
+        return when (request.path) {
+            "/weather.json" -> {
+                MockResponse()
+                    .setResponseCode(HttpURLConnection.HTTP_OK)
+                    .setBody(getJson("weather/weather.json"))
+            }
+            else -> throw IllegalArgumentException("Unknown Request Path ${request.path}")
+        }
+    }
+}
+
+fun getJson(path: String): String {
+    val uri = Resources.getResource(path)
+    val file = File(uri.path)
+    return String(file.readBytes())
+}
